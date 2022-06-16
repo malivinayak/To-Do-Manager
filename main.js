f3eeef7e84683f3a7ea9fe5ae9d3cc06ff0427c5
Vinayak Mali <malivinayak012@gmail.com>
2022-06-16 08:24:42 +0530

    Storing a name in local storage

--- main.js
+++ main.js
@@ -2,6 +2,15 @@ window.addEventListener('load', () => {
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
+    const nameInput = document.querySelector('#name');
+    //local storage
+    const username = localStorage.getItem('username') || '';
+    nameInput.value = username;
+
+    //Adding name to local storage
+    nameInput.addEventListener('change', (e) => {
+        localStorage.setItem('username', e.target.value);
+    })
 
     form.addEventListener('submit', (e) => {
         e.preventDefault();
