2e4919da002358e31be11ecf6b273bc1172c4994
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2022-12-23 22:27:12 +0530

    refactor: user name using local storage (#6)
    * refactor: separating sylesheet code for user name
    
    * refactor: separating js code for storing user name
    
    * refactor: removing leading and trailing whitespace
    
    * style: code formmating as per pritter
    
    * style: adding padding for greeting

---
+++ assets/name/storeName.js
@@ -0,0 +1,15 @@
+const nameInput = document.querySelector("#name");
+
+//local storage
+const username = localStorage.getItem("username").trim() || "";
+nameInput.value = username;
+
+//Adding name to local storage
+nameInput.addEventListener("change", (e) => {
+  /**
+   * @type {string} name
+   */
+  let name = e.target.value.trim();
+  nameInput.value = name;
+  localStorage.setItem("username", name);
+});
