3c83520b11cf1152f19740ee435dce80b332c323
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2022-12-24 11:33:17 +0530

    fix: warning for adding a task which is not blank (#10)

--- assets/custom-alert/alert.js
+++ assets/custom-alert/alert.js
@@ -1,25 +1,23 @@
-window.addEventListener("load", () => {
-  const form = document.querySelector("#new-task-form");
-  const input = document.querySelector("#new-task-input");
-  const alert = $("#custAlert");
-  const alertClose = $("#alertClose");
+const form = document.querySelector("#new-task-form");
+const input = document.querySelector("#new-task-input");
+const alert = $("#custAlert");
+const alertClose = $("#alertClose");
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    console.log(input.value);
-    if (input.value == "") {
-      alert.addClass("show");
-      alert.removeClass("hide");
-      alert.addClass("showAlert");
-      setTimeout(function () {
-        alert.removeClass("show");
-        alert.addClass("hide");
-      }, 5000);
-    }
-    alertClose.click(function () {
+  console.log(input.value);
+  if (input.value == "") {
+    alert.addClass("show");
+    alert.removeClass("hide");
+    alert.addClass("showAlert");
+    setTimeout(function () {
       alert.removeClass("show");
       alert.addClass("hide");
-    });
+    }, 5000);
+  }
+  alertClose.click(function () {
+    alert.removeClass("show");
+    alert.addClass("hide");
   });
 });
