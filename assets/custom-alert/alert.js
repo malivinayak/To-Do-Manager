ce8221dff901abb68bc82eedf0fb579449138924
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2022-12-24 11:25:36 +0530

    feat: warning notification (#8)
    giving warning notification while adding empty task

---
+++ assets/custom-alert/alert.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const alert = $("#custAlert");
+  const alertClose = $("#alertClose");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    console.log(input.value);
+    if (input.value == "") {
+      alert.addClass("show");
+      alert.removeClass("hide");
+      alert.addClass("showAlert");
+      setTimeout(function () {
+        alert.removeClass("show");
+        alert.addClass("hide");
+      }, 5000);
+    }
+    alertClose.click(function () {
+      alert.removeClass("show");
+      alert.addClass("hide");
+    });
+  });
+});
