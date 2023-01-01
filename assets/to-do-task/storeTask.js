1656c12c9c17694a8afb08e3407483befdb559e8
Vinayak Mali <66154908+malivinayak@users.noreply.github.com>
2023-01-01 11:29:20 +0530

    feat: Task saving in local storage (#13)

---
+++ assets/to-do-task/storeTask.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    /**
+     * @type {string} task
+     */
+    let new_task = input.value;
+    new_task = new_task.trim();
+    console.log(new_task + " from localStore");
+    if (new_task != "") {
+      if (localStorage.getItem("to-do-task") == null) {
+        localStorage.setItem("to-do-task", "[]");
+      }
+      /**
+       * @type {Array} task_list
+       */
+      var task_list = JSON.parse(localStorage.getItem("to-do-task"));
+      task_list.push(new_task);
+      localStorage.setItem("to-do-task", JSON.stringify(task_list));
+    }
+  });
+});
