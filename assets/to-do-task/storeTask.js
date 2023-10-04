52afae3e343eb5dee61a7d3e45f3dcad565617fd
Amrit Pandey <109029261+amripandey@users.noreply.github.com>
2023-10-04 14:41:19 +0000

    added delete alert

--- assets/to-do-task/storeTask.js
+++ assets/to-do-task/storeTask.js
@@ -102,6 +102,19 @@ window.addEventListener("load", () => {
 
           taskhead.removeChild(heading);
         }
+        const alert = $("#custAlert4");
+        const alertClose = $("#alertClose");
+        alert.addClass("show");
+        alert.removeClass("hide");
+        alert.addClass("showAlert");
+        setTimeout(function () {
+          alert.removeClass("show");
+          alert.addClass("hide");
+        }, 5000);
+        alertClose.click(function () {
+          alert.removeClass("show");
+          alert.addClass("hide");
+        });
       });
     }
   }
